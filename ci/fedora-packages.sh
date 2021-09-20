9f527fe9cbac53b3c78fbd89d50441e0c916cae2
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2021-09-20 15:31:27 +0200

    Install "epel-rpm-macros" for CC7 platform in build setup

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -5,7 +5,7 @@ if [[ -f /usr/bin/dnf ]]; then
   dnf install -y dnf-plugins-core git rpm-build which tree python2 \
                  cmake make gcc gcc-c++
 else
+  yum install -y epel-rpm-macros
   yum install -y yum-utils git rpm-build make which tree python2 \
                  cmake cmake3 make gcc gcc-c++
 fi
-
