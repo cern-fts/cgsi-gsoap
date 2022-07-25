a448d76b23ebeafcb574f0973a3f6aee07c35a86
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-07-25 11:31:32 +0300

    Remove "python2" package installation on platforms with "dnf" package manager

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -f /usr/bin/dnf ]]; then
-  dnf install -y dnf-plugins-core git rpm-build which tree python2 \
+  dnf install -y dnf-plugins-core git rpm-build which tree \
                  cmake make gcc gcc-c++
 else
   yum install -y epel-rpm-macros
